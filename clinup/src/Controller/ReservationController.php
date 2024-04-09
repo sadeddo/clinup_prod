de2d4aa66fc26fa28e545d15bb2c50039da6ead3
Oumayma Sadeddine <sadeddine_o@MacBook-Pro-de-Oumayma.local>
2024-04-09 02:33:23 +0000

    mod

--- clinup/src/Controller/ReservationController.php
+++ clinup/src/Controller/ReservationController.php
@@ -279,14 +279,14 @@ class ReservationController extends AbstractController
         $formattedAddrTo     = str_replace(' ', '+', $addressTo);
         
         // Geocoding API request with start address
-        $geocodeFrom = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$formattedAddrFrom.'&sensor=false&key='.$apiKey);
+        $geocodeFrom = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($formattedAddrFrom).'&sensor=false&key='.$apiKey);
         $outputFrom = json_decode($geocodeFrom);
         if(!empty($outputFrom->error_message)){
             return $outputFrom->error_message;
         }
         
         // Geocoding API request with end address
-        $geocodeTo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$formattedAddrTo.'&sensor=false&key='.$apiKey);
+        $geocodeTo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($formattedAddrTo).'&sensor=false&key='.$apiKey);
         $outputTo = json_decode($geocodeTo);
         if(!empty($outputTo->error_message)){
             return $outputTo->error_message;
