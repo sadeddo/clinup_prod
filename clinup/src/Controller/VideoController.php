fbe9075aa75b1d5772b4fbfd5d67bac3529ecc3d
sadedd_o <sadeddine_o@MacBook-Pro-de-Oumayma.local>
2024-06-27 13:46:52 +0100

    modifier

--- clinup/src/Controller/VideoController.php
+++ clinup/src/Controller/VideoController.php
@@ -19,48 +19,56 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 class VideoController extends AbstractController
 {
     #[Route('/video/{id}/upload', name: 'app_video_upload')]
-    public function index(Reservation $reservation, Request $request, EntityManagerInterface $entityManager, Security $security, ReservationRepository $reservationRepository,VideoRepository $videoRepository): Response
-    {
-        $video = new Video();
-        $form = $this->createForm(VideoType::class, $video);
+public function index(Reservation $reservation, Request $request, EntityManagerInterface $entityManager, Security $security, ReservationRepository $reservationRepository, VideoRepository $videoRepository): Response
+{
+    $video = new Video();
+    $form = $this->createForm(VideoType::class, $video);
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $file = $form->get('filePath')->getData();
-            if ($file) {
-                $filename = uniqid() . '.' . $file->guessExtension();
-                try {
-                    $file->move(
-                        $this->getParameter('videos_directory'),
-                        $filename
-                    );
-                    $video->setFilePath($filename);
-                    $video->setReservation($reservation);
-                    $entityManager->persist($video);
-                    $entityManager->flush();
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $file = $form->get('filePath')->getData();
+        if ($file) {
+            $filename = uniqid() . '.' . $file->guessExtension();
+            try {
+                $file->move(
+                    $this->getParameter('videos_directory'),
+                    $filename
+                );
+                $video->setFilePath($filename);
+                $video->setReservation($reservation);
+                $entityManager->persist($video);
+                $entityManager->flush();
 
+                if ($request->isXmlHttpRequest()) {
+                    return new JsonResponse(['message' => 'Vidéo uploadée avec succès !'], Response::HTTP_OK);
+                } else {
                     $this->addFlash('success', 'Vidéo uploadée avec succès !');
-                } catch (FileException $e) {
+                }
+            } catch (FileException $e) {
+                if ($request->isXmlHttpRequest()) {
+                    return new JsonResponse(['message' => 'Une erreur est survenue lors de l\'upload de la vidéo.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+                } else {
                     $this->addFlash('error', 'Une erreur est survenue lors de l\'upload de la vidéo.');
                 }
-                return $this->redirectToRoute('app_reservation_show', ['id' => $reservation->getId()], Response::HTTP_SEE_OTHER);
             }
+            return $this->redirectToRoute('app_reservation_show', ['id' => $reservation->getId()], Response::HTTP_SEE_OTHER);
         }
-
-        // Vérifier si l'utilisateur a déjà postulé
-        $postulation = $entityManager->getRepository(Postuler::class)
-            ->findOneBy(['reservation' => $reservation, 'prestataire' => $security->getUser()]);
-            $video = $videoRepository->findOneBy(['reservation' => $reservation]);
-        return $this->render('reservation/showP.html.twig', [
-            'form' => $form->createView(),
-            'cible' => 'ajouterVideo',
-            'idReservation' => $reservation->getId(),
-            'reservation' => $reservationRepository->findOneBy(['id' => $reservation->getId()]),
-            'hasApplied' => $postulation,
-            'video' => $video,
-        ]);
     }
-    // src/Controller/VideoController.php
+
+    // Vérifier si l'utilisateur a déjà postulé
+    $postulation = $entityManager->getRepository(Postuler::class)
+        ->findOneBy(['reservation' => $reservation, 'prestataire' => $security->getUser()]);
+    $video = $videoRepository->findOneBy(['reservation' => $reservation]);
+
+    return $this->render('reservation/showP.html.twig', [
+        'form' => $form->createView(),
+        'cible' => 'ajouterVideo',
+        'idReservation' => $reservation->getId(),
+        'reservation' => $reservationRepository->findOneBy(['id' => $reservation->getId()]),
+        'hasApplied' => $postulation,
+        'video' => $video,
+    ]);
+}
 
     #[Route('/video/{id}', name: 'app_video_show')]
     public function show(Video $video): Response
