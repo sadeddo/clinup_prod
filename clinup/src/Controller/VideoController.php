b708ea6a0cd65936a3061b3edf1caf892ac10bcf
sadedd_o <sadeddine_o@MacBook-Pro-de-Oumayma.local>
2024-06-27 13:52:54 +0100

    modifier

--- clinup/src/Controller/VideoController.php
+++ clinup/src/Controller/VideoController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
